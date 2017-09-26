cc18601e5bde46365fb4db912bfb5fa80d35ac92
Martin Preisler <martin@preisler.me>
2017-09-26 15:16:47 -0400

    Initial version of the run.sh script and HTML template

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -rf public_html
+cp -r template_public_html public_html
+pushd public_html
+wget https://jenkins.open-scap.org/job/static_openscap_docs/lastSuccessfulBuild/artifact/static.open-scap.org.zip
+unzip static.open-scap.org.zip
+popd
