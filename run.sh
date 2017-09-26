ed48ba00f1ab7cacfc610b122b221f51c08552f8
Martin Preisler <martin@preisler.me>
2017-09-26 15:17:28 -0400

    Clean up the ZIP file after it's extracted

--- run.sh
+++ run.sh
@@ -5,4 +5,5 @@ cp -r template_public_html public_html
 pushd public_html
 wget https://jenkins.open-scap.org/job/static_openscap_docs/lastSuccessfulBuild/artifact/static.open-scap.org.zip
 unzip static.open-scap.org.zip
+rm static.open-scap.org.zip
 popd
